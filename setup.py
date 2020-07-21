039955d127e3d7a07b070f8d6f3e13e95e344d34
Kavi Gupta <kavi@berkeley.edu>
2020-07-21 01:27:32 -0700

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skeletonize",
-    version="2.4",
+    version="2.5",
     author="Kavi Gupta",
     author_email="skeletonize@kavigupta.org",
     description="Handles skeletonization and deskeletonization of python code.",
